5580e02a617fd07f9b408ba551ed78e021b844c3
Kaung Htet Mon <kaunghtetmon456@gmail.com>
2025-01-24 16:47:38 +0900

    slice

---
+++ multiline.rb
@@ -0,0 +1,6 @@
+i=<<MLS
+  hi its me kaung htet mon
+  i am 22 years old
+MLS
+
+print i
---
+++ stringconcat.rb
@@ -0,0 +1,13 @@
+firstname= "harry"
+puts firstname+" potter"
+
+puts firstname<<" potter"
+
+puts firstname
+
+puts firstname.size
+puts firstname.length
+
+#slice
+puts firstname.slice(0,3)
+puts firstname[0,3]
