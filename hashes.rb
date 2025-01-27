b498ff6644b212838304252dbe402cca0dccdcbe
Kaung Htet Mon <kaunghtetmon456@gmail.com>
2025-01-27 14:09:10 +0900

    blue

---
+++ hashes.rb
@@ -0,0 +1,23 @@
+id={
+  "name"=>"kaunghtetmon",
+  "id"=>1
+}
+puts id["id"]
+
+#symbols
+#hash keys with symbols
+person={
+  name: "KHM",
+  age: 23
+}
+
+puts person[:name]
+
+
+#shorthand for hashes
+red= "red"
+green = "green"
+blue= "blue"
+
+colors={red:, green:, blue:}
+puts colors[:green]
