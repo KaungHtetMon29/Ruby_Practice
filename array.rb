47ba630346663565b8ba4b0e713e05a9cbd5fe2c
Kaung Htet Mon <kaunghtetmon456@gmail.com>
2025-01-27 10:07:23 +0900

    range and array

---
+++ array.rb
@@ -0,0 +1,19 @@
+arr=[1,2,3,4]
+# puts arr
+
+arr2=%w[1 2 3 4]
+# puts arr2
+
+arr2 << 5
+# puts arr2
+arr2.insert(2,10,20)
+# puts arr2
+
+arr2.pop(2)
+# puts arr2
+
+p arr2.shift(2)
+p arr2
+
+p arr2.unshift(2)
+p arr2
---
+++ range.rb
@@ -0,0 +1,10 @@
+inclusive_nums=1..5
+exclusive_nums=1...5 #exclude 5
+# puts inclusive_nums.last
+# puts exclusive_nums.last
+
+puts exclusive_nums.last(3)
+puts inclusive_nums.last(3)
+
+alpherbet= "A".."z"
+p alpherbet.first(40)
