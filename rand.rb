4f5b120ac0ff66eaf8fb0d65597495a4eb5f8d6b
Kaung Htet Mon <kaunghtetmon456@gmail.com>
2025-01-27 10:48:19 +0900

    random

---
+++ rand.rb
@@ -0,0 +1,8 @@
+puts rand 
+puts rand.round(2)
+puts
+puts rand(100)#exclude100
+puts rand(101) #include 100
+puts
+puts rand(1..50)#include 50
+puts rand(1...50)#exclude 50
--- range.rb
+++ range.rb
@@ -8,3 +8,33 @@ puts inclusive_nums.last(3)
 
 alpherbet= "A".."z"
 p alpherbet.first(40)
+
+p alpherbet.include?("a")
+p alpherbet.member?("a")
+p alpherbet==="a"
+
+
+#extract from string using range"
+
+story= "once upon a time in the galaxy far, far away..."
+
+
+p story.slice(1...10)
+p story[1...10]
+
+#case with range
+def rangeWithCase(marks)
+  case marks
+  when 0...40 then "D"
+  when 40...60 then "C"
+  when 60...80 then "B"
+  when 80..100 then "A"
+  else "F"
+  end
+end
+
+puts rangeWithCase(100)
+
+#range to array
+rToArr= 1...100
+p rToArr.to_a
